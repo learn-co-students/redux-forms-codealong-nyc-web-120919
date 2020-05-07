d5f534219f51d1cb7bd1a7ffd4c3416811bc3382
kennja05 <jacobkenny05@gmail.com>
2020-05-07 16:52:39 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,13 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch(action.type){
+    case 'ADD_TODO' :
+      console.log({ todos: state.todos.concat(action.payload.text) });
+ 
+      return { todos: state.todos.concat(action.payload.text) };
+ 
+    default:
+      return state;
+  }
 }
